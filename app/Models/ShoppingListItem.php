d5e762c75a1a04e9b1e37c6a331829a2f8887494
Joe Tannenbaum <joe@joe.codes>
2020-11-15 16:18:59 -0500

    sortable package

--- app/Models/ShoppingListItem.php
+++ app/Models/ShoppingListItem.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class ShoppingListItem extends Model
+class ShoppingListItem extends Model implements Sortable
 {
-    use HasFactory;
+    use SortableTrait, HasFactory;
 
     protected $fillable = [
         'order',
         'quantity',
     ];
+
+    public function buildSortQuery()
+    {
+        return static::query()->where('shopping_list_id', $this->shopping_list_id);
+    }
 
     public function item()
     {
--- app/Models/StoreTag.php
+++ app/Models/StoreTag.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class StoreTag extends Model
+class StoreTag extends Model implements Sortable
 {
-    use HasFactory;
+    use SortableTrait, HasFactory;
 
     protected $fillable = [
         'name',
         'order',
     ];
+
+    public function buildSortQuery()
+    {
+        return static::query()->where('store_id', $this->store_id);
+    }
 
     public function store()
     {
