1966b09fcecac0e5ce14f4ee4fc3203e25133bdc
Joe Tannenbaum <joe@joe.codes>
2020-11-16 10:42:28 -0500

    register endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ShoppingListController;
 use App\Http\Controllers\ShoppingListItemController;
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('auth/register', AuthController::class . '@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
