01db1197d2904a794f8b044ac62d35f5d15d9022
Joe Tannenbaum <joe@joe.codes>
2021-03-10 15:53:13 -0500

    first draft of both of these documents

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return null;
 });
 
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+});
+
+Route::get('/terms-and-conditions', function () {
+    return view('terms-and-conditions');
+});
+
 Route::get('/list/{uuid}', function () {
     return null;
 });
