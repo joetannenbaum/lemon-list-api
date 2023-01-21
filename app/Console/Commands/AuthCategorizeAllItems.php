fae17c78ae6df5c55548992b6d0d1121e0bcc9dc
Joe Tannenbaum <joe@joe.codes>
2023-01-20 21:45:19 -0500

    Update AuthCategorizeAllItems.php

--- app/Console/Commands/AuthCategorizeAllItems.php
+++ app/Console/Commands/AuthCategorizeAllItems.php
@@ -31,7 +31,7 @@ class AuthCategorizeAllItems extends Command
     {
         User::with('items')->find([1, 6])->each(
             fn ($u) => $u->items->each(
-                fn ($i, $index) => dispatch(new AutoCategorizeItem($i))->delay(now()->addSeconds($index + 1))
+                fn ($i, $index) => dispatch(new AutoCategorizeItem($i))->delay(now()->addSeconds($index * 2))
             )
         );
 
